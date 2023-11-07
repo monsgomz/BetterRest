8b48f5d9c37849576c5364158e40f57405e28578
M Montserrat Gomez <gome0145@algonquinlive.com>
2023-11-07 17:10:49 -0500

    Create function to connect with CoreML

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -64,6 +64,21 @@ struct ContentView: View {
 	/// Funcion para conectar con CoreML
 	func calculateBedtime() {
 		
+		do {
+			let config = MLModelConfiguration()
+			let model = try SleepCalculator(configuration: config)
+			let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+			let hour = (components.hour ?? 0) * 60 * 60
+			let minute = (components.minute ?? 0) * 60
+			
+			//dice cuanto tiempo debe dormir
+			let prediction = try model.prediction(wake: Int64((hour + minute)), estimatedSleep: sleepAmount, coffee: Int64((coffeeAmount)))
+			
+			let sleepTime = wakeUp - prediction.actualSleep
+			
+		} catch {
+			// something went wrong!
+		}
 	}
 }
 
